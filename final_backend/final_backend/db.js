bc1b719385ea41e3ce74fb096304ede61a27db07
ba bao <luubao@gmail.com>
2026-09-10 21:39:32 +0700

    Fix SSL handshake error

--- final_backend/final_backend/db.js
+++ final_backend/final_backend/db.js
@@ -6,9 +6,9 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT, // CỰC KỲ QUAN TRỌNG: Phải có dòng này!
+    port: process.env.DB_PORT,
     ssl: {
-        rejectUnauthorized: true // Vẫn phải giữ nguyên dòng này nhé
+        rejectUnauthorized: false // CHỈ CẦN SỬA CHỮ NÀY THÀNH FALSE
     }
 });
 
